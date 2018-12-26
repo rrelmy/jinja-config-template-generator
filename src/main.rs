2b3c2fba16fe18a96a79e09c2834835ceb31ceb3
Rémy <git@rrelmy.ch>
2018-12-26 22:28:24 +0100

    variable type for escaping string

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,13 @@ enum ConfigLevel {
     SubSection,
 }
 
+#[derive(PartialEq)]
+enum VariableType {
+    Boolean,
+    String,
+    Transparent, // no escaping is applied, numbers for example
+}
+
 fn normalized_line(line: &str) -> String {
     let mut line = line.trim();
 
@@ -42,14 +49,30 @@ fn is_config_line(line: &str) -> bool {
     true
 }
 
-fn parse_config_line(line: &str) -> (String, String) {
+fn type_of_value(value: &str) -> VariableType {
+    if value == "true" || value == "false" {
+        return VariableType::Boolean;
+    }
+
+    if value.starts_with('"') {
+        return VariableType::String;
+    }
+
+    VariableType::Transparent
+}
+
+fn parse_config_line(line: &str) -> (String, String, VariableType) {
     // TODO &str would be better
     let spl: Vec<&str> = line.split(" = ").collect();
     if spl.len() != 2 {
         panic!("Could not parse config line");
     }
 
-    (spl[0].to_owned(), spl[1].to_owned())
+    let key = spl[0].to_owned();
+    let value = spl[1].to_owned();
+    let variable_type = type_of_value(&value);
+
+    (key, value, variable_type)
 }
 
 fn get_line_prefix(level: &ConfigLevel) -> String {
@@ -85,8 +108,8 @@ fn get_variable_name(
     var
 }
 
-fn config_value(value: &str) -> String {
-    if value == "true" || value == "false" {
+fn escape_config_value(value: &str, variable_type: &VariableType) -> String {
+    if variable_type == &VariableType::Boolean {
         let mut result = String::new();
         result.push('"');
         result.push_str(value);
@@ -96,6 +119,34 @@ fn config_value(value: &str) -> String {
     }
 
     String::from(value)
+}
+
+fn variable_line(
+    prefix: &str,
+    name: &str,
+    variable_name: &str,
+    variable_type: &VariableType,
+) -> String {
+    let mut result = String::new();
+
+    result.push_str(prefix);
+    result.push_str(name);
+
+    result.push_str(" = ");
+
+    if variable_type == &VariableType::String {
+        result.push_str("\"");
+    }
+
+    result.push_str("{{ ");
+    result.push_str(variable_name);
+    result.push_str(" }}");
+
+    if variable_type == &VariableType::String {
+        result.push_str("\"");
+    }
+
+    result
 }
 
 fn main() {
@@ -141,7 +192,7 @@ fn main() {
             variables.push_str(&section);
             variables.push_str(&"]\n");
         } else if is_config_line(&line) {
-            let (name, value) = parse_config_line(&line);
+            let (name, value, variable_type) = parse_config_line(&line);
             //println!("Found config:\n  name: {}\n  value: {}\n", name, value)
 
             let variable_name =
@@ -149,12 +200,12 @@ fn main() {
 
             variables.push_str(&variable_name);
             variables.push_str(&": ");
-            variables.push_str(&config_value(&value));
+            variables.push_str(&escape_config_value(&value, &variable_type));
             variables.push_str(&"\n");
 
             let prefix = get_line_prefix(&level);
 
-            resulting_line = prefix + &name + " = {{ " + &variable_name + " }}";
+            resulting_line = variable_line(&prefix, &name, &variable_name, &variable_type);
         }
 
         // add content to target file
